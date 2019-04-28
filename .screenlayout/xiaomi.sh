5825dd6127d976a6e9ea23d32ff52c8365902037
leihu xi <leihuxi@gmail.com>
2019-04-28 10:28:52 +0800

    update

--- .screenlayout/xiaomi.sh
+++ .screenlayout/xiaomi.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1366x768 --pos 0x1080 --rotate normal --output DP1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+xrandr --output eDP-1 --primary --mode 1366x768 --pos 0x1080 --rotate normal --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off
