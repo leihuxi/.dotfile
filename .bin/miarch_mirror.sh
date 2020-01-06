5261906e2591447b75f706862d544bf1e56e4809
leihu xi <leihuxi@gmail.com>
2020-01-06 10:35:55 +0800

    update

--- .bin/miarch_mirror.sh
+++ .bin/miarch_mirror.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --same-as eDP-1
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --same-as eDP1
