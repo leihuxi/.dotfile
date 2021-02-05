50964e855249b86a562cc4e6208e8f6248e55064
leihu xi <leihuxi@gmail.com>
2021-02-05 13:33:34 +0800

    fix

--- .bin/parted_disk.sh
+++ .bin/parted_disk.sh
@@ -7,36 +7,40 @@ if [ $# -ne 2 ]; then
     info "./parted_disk.sh {/dev/sda} {split}"
     exit
 fi
+disk="$1"
+disksplit="$2"
 
 trap 'custom_exit; exit' SIGINT SIGQUIT
 custom_exit() {
     error "you hit Ctrl-C/Ctrl-\, now exiting.."
 }
 ##安装参数,适合一个硬盘
-disk="$1"
-disksplit="$2"
 bios_order=1
 boot_order=2
 swap_order=3
 rootfs_order=4
 home_order=5
-boot_size=259
-swap_size=16
-rootfs_size=128
+
+bios_size=2
+boot_size=256
+swap_size=$((16 * 1024))
+rootfs_size=$((128 * 1024))
 
 diskprefix="${disk}${disksplit}"
 home="${diskprefix}${home_order}"
 rootfs="${diskprefix}${rootfs_order}"
 swap="${diskprefix}${swap_order}"
 boot="${diskprefix}${boot_order}"
+bios="${diskprefix}${bios_order}"
 
-bios_end=3
-boot_end=$boot_size
-swap_end=$(($boot_end + $swap_size * 1024))
-rootfs_end=$(($swap_end + $rootfs_size * 1024))
+bios_end=1 + $bios_size
+boot_end=$bios_end+$boot_size
+swap_end=$($boot_end + $swap_size)
+rootfs_end=$($swap_end + $rootfs_size)
 
-info "home=${home}, boot=${boot}, rootfs=${rootfs}, swap=${swap}"
-info "boot=1Mib, ${boot_end}Mib"
+info "home=${home}, boot=${boot}, rootfs=${rootfs}, swap=${swap} bios=${bios}"
+info "boot=1Mib, ${bios_end}Mib"
+info "boot=${bios_end}Mib, ${boot_end}Mib"
 info "swap=${boot_end}Mib, ${swap_end}Mib"
 info "rootfs=${swap_end}Mib, ${rootfs_end}Mib"
 info "home=${rootfs_end}Mib, 100%"
@@ -44,8 +48,8 @@ info "home=${rootfs_end}Mib, 100%"
 umount -R /mnt
 parted -a optimal "${disk}" mklabel gpt
 parted -a optimal "${disk}" mkpart primary fat32 1Mib "${bios_end}"Mib
-parted -a optimal "${disk}" set "${bios_order}" bios_grub on
 parted -a optimal "${disk}" name "${bios_order}" grub
+parted -a optimal "${disk}" set "${bios_order}" bios_grub on
 parted -a optimal "${disk}" mkpart primary fat32 3Mib "${boot_end}"Mib
 parted -a optimal "${disk}" name "${boot_order}" boot
 parted -a optimal "${disk}" set "${boot_order}" boot on
